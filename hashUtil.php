23615d76228efb58c10ce8a9902a0b8b3daae9c7
Edward Januszewski <e.januszewski@gmail.com>
2013-05-13 23:23:20 +0100

    Merged and added enhancement
    Merged hashLoop into createHash and added option to specify hash length

--- hashUtil.php
+++ hashUtil.php
@@ -1,18 +1,14 @@
 <?php
 
 class HashUtil {
-	//Generates a url hash
-	private static function hashLoop() {
+	//Creates a hash
+	public function createHash($url, $length) {
 		$returnHash = "";
 		$chars = range("a","z");
-		while(strlen($returnHash) < 5) {
+		while(strlen($returnHash) < $length) {
 			$returnHash .= $chars[rand(0,count($chars)-1)];
 		}
-		echo $returnHash;
-	}
-	//Creates a hash
-	public function createHash($url) {
-		self::hashLoop();
+		return $returnHash;
 	}
 	//	$num = mysql_num_rows(mysql_query("SELECT `hash` FROM `url` WHERE `hash` = '".$returnHash."'"));
 	//	if ($num == 0) { echo $returnHash;} else {hashLoop();}
